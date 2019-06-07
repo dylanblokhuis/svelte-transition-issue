998542bf291aae3944a89ebfc974c7eb305bcda1
BlokyKappa <dylanblokhuis3@hotmail.com>
2019-06-07 16:03:01 +0200

    First commit

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { fade } from 'svelte/transition';
+
+	let visible = true;
+</script>
+
+<svelte:options tag="my-component" />
+
+<label>
+	<input type="checkbox" bind:checked={visible}>
+	visible
+</label>
+
+{#if visible}
+	<p transition:fade>
+		Fades in and out
+	</p>
+{/if}
+
